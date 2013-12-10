6353a58d0a6c1d47f7f628c266df5e5fecdfbf30
gd7627a <gd7627a@student.american.edu>
2013-12-09 21:32:26 -0800

    Refactored code to reflect module pattern and separted data binding from html mockup

--- mensa.js
+++ mensa.js
@@ -1,42 +1,64 @@
-// 4. Isolate Changing elements
-//   I. Question and Choice display change
-     // II. input values
-     // II. Input types
+Releases
+-Change internal logic so its not necessary to change the correct Answer  or use + 5 to similuate the difference between the text id and button id
+-Add Score Keeper
+-Implement Tests
+-Add Tracking Bar to show progress
+-Include Backend to Store
+   I. User Data
+   II. Data on Geniuses
+-Implement Design
 
 
 var QuizFeatures = {
+  init: function() {
+    this.button()
+
+  },
   button: function () {
+    var addButton = document.getElementById("button")
     var formButton = document.createElement("Button")
     formButton.setAttribute("id", "button")
     formButton.innerHTML = "next"
-    var addButton = document.getElementById("button")
     addButton.appendChild(formButton)
     },
-  quizQuestions: [{question: "Who is Prime Minister of the United Kingdom?", choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], correctAnswer:"0"},{question: "Who is President of the United States?", choices: ["George Bush", "Dave Chappelle", "Barack Obama ", "Jane Doe"], correctAnswer:"2"}, {question: "Who is President of the Republic of South Africa", choices: ["George Bush", "Dave Chappelle", "Barack Obama ", "Rick Ross"], correctAnswer:"2"} ],
+  quizQuestions: [{question: "Who is Prime Minister of the United Kingdom?", choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], correctAnswer:"5"},{question: "Who is President of the United States?", choices: ["George Bush", "Dave Chappelle", "Barack Obama ", "Jane Doe"], correctAnswer:"6"}, {question: "Who is President of the Republic of South Africa", choices: ["George Bush", "Dave Chappelle", "Barack Obama ", "Rick Ross"], correctAnswer:"6"} ],
   score: 0
 }
+QuizFeatures.init()
+
 var questionNumber = 0
+createSkeleton = function() {
+  for (var i = 0; i < QuizFeatures.quizQuestions[questionNumber].choices.length; i++) {
+    var questionDisplay = document.getElementById('question-display')
+    var answer = document.getElementById("answers")
+    var answerDisplay = document.createElement("text")
+    var quizChoice = document.createElement("input")
+    var br = document.createElement("br");
+    quizChoice.setAttribute("type", "radio")
+    quizChoice.setAttribute("name", "choice")
+    quizChoice.setAttribute("type", "radio")
+    quizChoice.setAttribute("name", "choice")
+    quizChoice.setAttribute("id",  i + 5)
+    answerDisplay.setAttribute("id", i)
+    answer.appendChild(quizChoice)
+    answer.appendChild(answerDisplay)
+    answer.appendChild(br)
+  }
+}
+
 
 createNextQuestion = function(questionNumber) {
   for (var i = 0; i < QuizFeatures.quizQuestions[questionNumber].choices.length; i++) {
     var questionDisplay = document.getElementById('question-display')
-    var br = document.createElement("br");
-    var quizChoice = document.createElement("input")
-    var answerDisplay = document.createElement("text")
-    var answer = document.getElementById("answers")
-    quizChoice.setAttribute("type", "radio")
-    quizChoice.setAttribute("name", "choice")
-    quizChoice.setAttribute("id",  i)
-    quizChoice.setAttribute("value", QuizFeatures.quizQuestions[questionNumber].choices[i])
-    answer.appendChild(quizChoice)
+    answerDisplay = document.getElementById(i)
     questionDisplay.innerHTML = QuizFeatures.quizQuestions[questionNumber].question
     answerDisplay.innerHTML = QuizFeatures.quizQuestions[questionNumber].choices[i]
-    answer.appendChild(answerDisplay)
-    answer.appendChild(br)
  }
 }
+createSkeleton();
+createNextQuestion(0)
+
 QuizFeatures.button()
-createNextQuestion(questionNumber);
 
 
 $("#button").on("click", function() {
@@ -45,6 +67,7 @@ $("#button").on("click", function() {
     questionNumber++
     QuizFeatures.score++
     $('#answers').empty()
+    createSkeleton(questionNumber)
     createNextQuestion(questionNumber)
   }
 });
