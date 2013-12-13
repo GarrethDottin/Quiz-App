8d8fefc99a71c089e9c32452388040abcda8fcbe
gd7627a <gd7627a@student.american.edu>
2013-12-12 23:05:13 -0800

    Separated data binding from markup

--- mensa.js
+++ mensa.js
@@ -1,7 +1,5 @@
 // Ways to Refactor
-// -Separate Data Creation from Data binding
-    //I. Go over expected behavior of functions
-    //II. finish create Skeleton (create elements separate)
+//
 // -separate on.click anonymous function
 // -add an onload with .init() that calls the listener
 // - change markup to a return function
@@ -14,8 +12,12 @@
 //
 
 var QuizFeatures = {
-  init: function() {
+  init: function(questionNumber) {
     this.button()
+    this.createSkeleton(0)
+    $("#button1").on("click", function() {
+  alert("this is working")})
+
   },
   markup:
     {
@@ -32,46 +34,33 @@ var QuizFeatures = {
     this.markup.formButton.innerHTML = "next"
     this.markup.addButton.appendChild(this.markup.formButton)
     },
-
-  questionMarkup: function(i) {
-    console.log(this.markup.answerDisplay)
-    this.markup.quizChoice.setAttribute("type", "radio")
-    this.markup.quizChoice.setAttribute("id", i)
-    this.markup.answer.appendChild(this.markup.quizChoice)
-    this.markup.answer.appendChild(this.markup.answerDisplay)
-    this.markup.answer.appendChild(this.markup.br)
-  },
   quizQuestions: [{question: "Who is Prime Minister of the United Kingdom?", choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], correctAnswer:"5"},{question: "Who is President of the United States?", choices: ["George Bush", "Dave Chappelle", "Barack Obama ", "Jane Doe"], correctAnswer:"7"}, {question: "Who is President of the Republic of South Africa", choices: ["George Bush", "Dave Chappelle", "Barack Obama ", "Rick Ross"], correctAnswer:"6"} ],
   score: 0,
   createSkeleton: function(num) {
-    for (var i = 0; i < QuizFeatures.quizQuestions[questionNumber].choices.length; i++) {
-      QuizFeatures.questionMarkup(i)
-      QuizFeatures.markup.quizChoice.setAttribute("id",  i)
-      QuizFeatures.markup.answerDisplay.setAttribute("id", i)
-      QuizFeatures.markup.answer.appendChild(QuizFeatures.markup.quizChoice)
-      QuizFeatures.markup.answer.appendChild(QuizFeatures.markup.answerDisplay)
-      QuizFeatures.markup.answer.appendChild(QuizFeatures.markup.br)
+    for (var i = 0; i < QuizFeatures.quizQuestions[num].choices.length; i++) {
+      var choice = document.createElement("input");
+      var answer = document.createElement("text");
+      var br = document.createElement("br");
+      choice.setAttribute("type", "radio")
+      choice.setAttribute("id",  i)
+      answer.setAttribute("id", i + "answer")
+      this.markup.answer.appendChild(choice)
+      this.markup.answer.appendChild(answer)
+      this.markup.answer.appendChild(br)
+      createNextQuestion(num, i)
     }
   }
 }
-QuizFeatures.init()
-var questionNumber = 0
+QuizFeatures.init(questionNumber)
 
-createNextQuestion = function(questionNumber) {
-  for (var i = 0; i < QuizFeatures.quizQuestions[questionNumber].choices.length; i++) {
-    var questionDisplay = document.getElementById('question-display')
-    answerDisplay = document.getElementById(i)
-    questionDisplay.innerHTML = QuizFeatures.quizQuestions[questionNumber].question
-    answerDisplay.innerHTML = QuizFeatures.quizQuestions[questionNumber].choices[i]
+function createNextQuestion(num, i) {
+  var questionDisplay = document.getElementById('question-display')
+  answerDisplay = document.getElementById(i + "answer")
+  questionDisplay.innerHTML = QuizFeatures.quizQuestions[num].question
+  answerDisplay.innerHTML = QuizFeatures.quizQuestions[num].choices[i]
+
  }
-}
-  createNextQuestion(0)
 
-
-
-
-$("#button1").on("click", function() {
-  alert("this is working")
   // var choice = $(':checked')[0].id
   // if (choice === QuizFeatures.quizQuestions[questionNumber].correctAnswer) {
   //   questionNumber++
@@ -80,4 +69,4 @@ $("#button1").on("click", function() {
   //   QuizFeatures.createSkeleton(questionNumber)
   //   createNextQuestion(questionNumber)
   // }
-});
+
